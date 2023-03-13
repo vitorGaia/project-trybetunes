3eb1d18be1bcc6f5606d5168c0c1e23954ce6115
vitorGaia <vitorgaia0027@gmail.com>
2023-03-13 18:32:31 -0300

    finaliza req 5, adicionei input, button e lógica da habilitar o botão no componente search

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,53 @@ import React from 'react';
 import Header from './Header';
 
 class Search extends React.Component {
+  state = {
+    searchArtistInput: '',
+    searchArtistButton: true,
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+    }, this.ableBtn);
+  };
+
+  ableBtn = () => {
+    const { searchArtistInput } = this.state;
+    const minLength = 2;
+    if (searchArtistInput.length >= minLength) {
+      this.setState({ searchArtistButton: false });
+    } else {
+      this.setState({ searchArtistButton: true });
+    }
+  };
+
   render() {
+    const { searchArtistButton } = this.state;
     return (
       <div data-testid="page-search">
         <Header />
-        <span>search</span>
+        <div>
+          <form>
+            <div>
+              <input
+                data-testid="search-artist-input"
+                type="text"
+                placeholder="Pesquisar"
+                name="searchArtistInput"
+                onChange={ this.handleChange }
+              />
+            </div>
+            <div>
+              <button
+                data-testid="search-artist-button"
+                disabled={ searchArtistButton }
+              >
+                Pesquisar
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
     );
   }
