57163081a395e85c0f54aecf28918f79a422d9be
vitorGaia <vitorgaia0027@gmail.com>
2023-04-17 09:53:38 -0300

    HashRouter

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ class App extends React.Component {
         <Switch>
           <Route
             exact
-            path=""
+            path="/"
             component={ Loguin }
           />
           <Route
